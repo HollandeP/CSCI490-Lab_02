aff4e5697d89df88b680cc76a73b16d124041f2a
Hollande <hollandepowell@gmail.com>
2020-02-05 18:21:52 -0500

    Initial Commit Second activity created

---
+++ app/src/main/java/com/example/csci370lab2/SelectBackground.java
@@ -0,0 +1,14 @@
+package com.example.csci370lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectBackground extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_background);
+    }
+}
