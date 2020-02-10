8dde2c9d39484c111f152ef3ceb08fbdb6e01d76
Hollande <hollandepowell@gmail.com>
2020-02-10 18:46:45 -0500

    lab finished

--- app/src/main/java/com/example/csci370lab2/SelectBackground.java
+++ app/src/main/java/com/example/csci370lab2/SelectBackground.java
@@ -2,13 +2,50 @@ package com.example.csci370lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SelectBackground extends AppCompatActivity {
+    private ImageView supermoon;
+    private ImageView waterfall;
+    private int imgId;
+    private int RESPONSE_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_background);
+
+        final Intent intent = new Intent();
+        supermoon = findViewById(R.id.supermoonImage);
+        waterfall = findViewById(R.id.waterfallImage);
+
+        supermoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgId = R.drawable.supermoon;
+
+                intent.putExtra("imgId", imgId);
+                setResult(RESPONSE_CODE, intent);
+                finish();
+            }
+        });
+
+        waterfall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgId = R.drawable.waterfall;
+
+                intent.putExtra("imgId", imgId);
+                setResult(RESPONSE_CODE, intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
